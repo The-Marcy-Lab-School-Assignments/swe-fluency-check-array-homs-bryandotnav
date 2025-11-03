8ec044d9dc00284a762229e8c8cdeb2b050e05f9
Bryan <bryan001700@gmail.com>
2025-11-03 09:45:17 -0500

    completed problem 4

--- from-scratch.js
+++ from-scratch.js
@@ -31,8 +31,9 @@ const sampleNumbers = [7, 3, 9, 1, 4, 8, 2, 10, 5, 6];
 
 // Problem 4
 const getEvenNumbers = (nums) => {
-
+  return nums.filter((even)=>even % 2 === 0)
 };
+console.log(getEvenNumbers(sampleNumbers));
 
 // Problem 5
 const getLargestNumber = (numbers) => {
